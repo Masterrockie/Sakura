4465c98016349866cfadf9eb53cbcd2fc79c6e0a
Amal nath <96761219+Masterrockiei@users.noreply.github.com>
2022-01-06 21:59:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Sakura
 fi
-cd /EvaMaria
+cd /Sakura
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
